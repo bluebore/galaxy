d32a3dc4aff6401d8b065160981bf84d239a6678
imotai <codego.me@gmail.com>
2016-01-15 22:51:42 +0800

    add get host name

--- optools/settings.py
+++ optools/settings.py
@@ -2,7 +2,8 @@
 # Copyright (c) 2015, Galaxy Authors. All Rights Reserved
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
-NEXUS_SERVERS="cp01-ocean-893.epc.baidu.com:8868,cp01-ocean-893.epc.baidu.com:8869,cp01-ocean-893.epc.baidu.com:8870,cp01-ocean-893.epc.baidu.com:8871,cp01-ocean-893.epc.baidu.com:8872"
+import socket
+host_name = socket.gethostname()
+NEXUS_SERVERS="%s:8868,%s:8869,%s:8870,%s:8871,%s:8872"%(host_name, host_name, host_name, host_name, host_name)
 USER_PREFIX="/baidu/galaxy/users"
 QUOTA_PREFIX="/baidu/galaxy/quotas"
