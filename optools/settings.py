11e5eb72ecd19a4f01092e53989339b02530f429
imotai <codego.me@gmail.com>
2016-01-15 13:09:04 +0800

    add settings.py

---
+++ optools/settings.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+# Copyright (c) 2015, Galaxy Authors. All Rights Reserved
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+NEXUS_SERVERS="cp01-ocean-893.epc.baidu.com:8868,cp01-ocean-893.epc.baidu.com:8869,cp01-ocean-893.epc.baidu.com:8870,cp01-ocean-893.epc.baidu.com:8871,cp01-ocean-893.epc.baidu.com:8872"
+USER_PREFIX="/baidu/galaxy/users"
+QUOTA_PREFIX="/baidu/galaxy/quotas"
